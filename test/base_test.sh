42194a0e153329a9d68aa7a8ad7fa0cf3181c436
imos <github@imoz.jp>
2014-08-03 03:48:13 +0900

    Add a test for BASH3's bug.

--- test/base_test.sh
+++ test/base_test.sh
@@ -27,3 +27,14 @@ test::compare_string() {
     LOG ERROR 'string comparation is bad: [ "a" < "aa" ]'
   fi
 }
+
+# CAVEATS:
+#     BASH 3 has a bug: $'\x01' in array is treated as $'\x01\x01'.
+test::soh_in_array() {
+  local values=($'\x01')
+  local expected='01'
+  if [ "${expected}" != "$(php::bin2hex "$(php::implode '' values)")" ]; then
+    LOG ERROR 'soh cannot be treated in array correctly:' \
+              "$(php::bin2hex "$(php::implode '' values)")"
+  fi
+}
