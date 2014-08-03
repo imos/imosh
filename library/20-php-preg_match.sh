8086b8191592e38cc21cf4f2fcb1583d7b557dba
imos <github@imoz.jp>
2014-08-04 05:22:44 +0900

    Enable persistent php.

--- library/20-php-preg_match.sh
+++ library/20-php-preg_match.sh
@@ -4,28 +4,14 @@ php::preg_match() {
   local __preg_match_name=''
 
   if [ "$#" -ge 3 ]; then __preg_match_name="${3}"; fi
-  local __preg_match_result="$(php << EOM 
-<?php
-if (version_compare(PHP_VERSION, '5.2.4') >= 0) {
-  ini_set('display_errors', 'stderr');
-} else {
-  error_reporting(0);
-}
-
-if (preg_match('$(php::addslashes "${__preg_match_pattern}")',
-               '$(php::addslashes "${__preg_match_subject}")',
-               \$match)) {
-  echo '1' . implode("\\x02", \$match);
-} else {
-  echo '0';
-}
-EOM
-  )"
-  if [ "${__preg_match_result}" == '0' ]; then
+  local __preg_match_result
+  if ! php::internal::run "
+      if (!preg_match('$(php::addslashes "${__preg_match_pattern}")',
+                      '$(php::addslashes "${__preg_match_subject}")',
+                      \$match)) { return 1; }
+      echo implode(\"\\x02\", \$match); return 0;" __preg_match_result; then
     return 1
-  fi
-  if [ "${__preg_match_name}" != '' ]; then
-    __preg_match_result="${__preg_match_result:1}"
+  else
     php::explode "${__preg_match_name}" $'\x02' "${__preg_match_result}"
   fi
 }
