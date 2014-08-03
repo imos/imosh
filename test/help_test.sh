a8064dc87e402d942236bb26b4cde50a1c6bc2a1
imos <github@imoz.jp>
2014-08-03 22:06:56 +0900

    Add a test for help flags.

---
+++ test/help_test.sh
@@ -0,0 +1,13 @@
+test::help() {
+  local expected_message="Usage: test/flags.sh [options ...] [args ...]
+Options:
+  --help=false: Print this help message and exit. (Alias: --h)
+  --flag='': Flag name to show.
+  --string='default': String flag.
+  --int=100: Integer flag.
+  --bool=false: Boolean flag."
+  EXPECT_EQ "${expected_message}" "$(bash test/flags.sh --help 2>&1 >/dev/null)"
+  EXPECT_EQ "${expected_message}" "$(bash test/flags.sh -h 2>&1 >/dev/null)"
+  # There should be no output to the standard output.
+  EXPECT_EQ '' "$(bash test/flags.sh --help 2>/dev/null)"
+}
