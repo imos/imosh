fdcdb629650d9e4ac563c3bf43afe7722c7c452e
imos <github@imoz.jp>
2014-08-03 18:41:08 +0900

    Add test cases for string flags and int flags.

--- test/flags_test.sh
+++ test/flags_test.sh
@@ -43,3 +43,49 @@ test::bool_flag() {
     fi
   done
 }
+
+test::int_flag() {
+  local pids=()
+  EXPECT_EQ 'FLAGS_int=100' "$(get_flag --flag=int)" &
+  pids+=("$!")
+
+  EXPECT_EQ 'FLAGS_int=0' "$(get_flag --flag=int --int=0)" &
+  pids+=("$!")
+  EXPECT_EQ 'FLAGS_int=12345' "$(get_flag --flag=int --int=12345)" &
+  pids+=("$!")
+  EXPECT_EQ 'FLAGS_int=-12345' "$(get_flag --flag=int --int=-12345)" &
+  pids+=("$!")
+  EXPECT_EQ 'FLAGS_int=012345' "$(get_flag --flag=int --int=012345)" &
+  pids+=("$!")
+
+  EXPECT_EQ 'invalid' "$(get_flag --flag=int --int)" &
+  pids+=("$!")
+  EXPECT_EQ 'invalid' "$(get_flag --flag=int --int=abc)" &
+  pids+=("$!")
+
+  for pid in "${pids[@]}"; do
+    if ! wait "${pid}"; then
+      IMOSH_TEST_IS_FAILED=1
+    fi
+  done
+}
+
+test::string_flag() {
+  local pids=()
+  EXPECT_EQ 'FLAGS_string=default' "$(get_flag --flag=string)" &
+  pids+=("$!")
+
+  EXPECT_EQ 'FLAGS_string=' "$(get_flag --flag=string --string=)" &
+  pids+=("$!")
+  EXPECT_EQ 'FLAGS_string=abc' "$(get_flag --flag=string --string=abc)" &
+  pids+=("$!")
+
+  EXPECT_EQ 'invalid' "$(get_flag --flag=string --string)" &
+  pids+=("$!")
+
+  for pid in "${pids[@]}"; do
+    if ! wait "${pid}"; then
+      IMOSH_TEST_IS_FAILED=1
+    fi
+  done
+}
