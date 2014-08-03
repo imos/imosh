327f33d91892d5f475d16a833e8d0b298fedbd4b
imos <github@imoz.jp>
2014-08-03 22:10:23 +0900

    Speed up test::help.

--- test/help_test.sh
+++ test/help_test.sh
@@ -1,4 +1,5 @@
 test::help() {
+  local pids=()
   local expected_message="Usage: test/flags.sh [options ...] [args ...]
 Options:
   --help=false: Print this help message and exit. (Alias: --h)
@@ -6,8 +7,20 @@ Options:
   --string='default': String flag.
   --int=100: Integer flag.
   --bool=false: Boolean flag."
-  EXPECT_EQ "${expected_message}" "$(bash test/flags.sh --help 2>&1 >/dev/null)"
-  EXPECT_EQ "${expected_message}" "$(bash test/flags.sh -h 2>&1 >/dev/null)"
+  EXPECT_EQ "${expected_message}" \
+            "$(bash test/flags.sh --help 2>&1 >/dev/null)" &
+  pids+=("$!")
+  EXPECT_EQ "${expected_message}" \
+            "$(bash test/flags.sh -h 2>&1 >/dev/null)" &
+  pids+=("$!")
+
   # There should be no output to the standard output.
-  EXPECT_EQ '' "$(bash test/flags.sh --help 2>/dev/null)"
+  EXPECT_EQ '' "$(bash test/flags.sh --help 2>/dev/null)" &
+  pids+=("$!")
+
+  for pid in "${pids[@]}"; do
+    if ! wait "${pid}"; then
+      IMOSH_TEST_IS_FAILED=1
+    fi
+  done
 }
