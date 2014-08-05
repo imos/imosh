5c4c4e2eb89e976a390afa9e61f3ed0d5c8a754e
imos <github@imoz.jp>
2014-08-06 02:09:58 +0900

    Remove a test having a trailing new line.

--- test/php_ltrim_test.sh
+++ test/php_ltrim_test.sh
@@ -1,7 +1,6 @@
 test::php::ltrim() {
   EXPECT_EQ 'abc def  ' "$(php::ltrim '  abc def  '; echo)"
-  EXPECT_EQ $'abc def\n' "$(php::ltrim $'  abc def\n'; echo)"
-  EXPECT_EQ 'abc def  ' "$(php::ltrim $'\t\n\r \t\n\rabc def  ')"
+  EXPECT_EQ 'abc def  ' "$(php::ltrim $'\t\n\r \t\n\rabc def  '; echo)"
   EXPECT_EQ 'abc def' "$(php::ltrim $'abc def')"
   EXPECT_EQ '' "$(php::ltrim $'')"
   EXPECT_EQ '' "$(php::ltrim $' ')"
