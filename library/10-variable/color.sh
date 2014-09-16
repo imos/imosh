6f7844d11acb00d01b2e7b5d8086a788724c33e8
imos <github@imoz.jp>
2014-09-17 03:33:09 +0900

    Refactor 20-color.sh and move it to 10-variable/color.sh.

---
+++ library/10-variable/color.sh
@@ -0,0 +1,13 @@
+# Color definitions.  A shell script should restore terminal's original color
+# using IMOSH_STYLE_DEFAULT when it changes color or style.
+
+IMOSH_STYLE_DEFAULT=$'\033[0m'
+IMOSH_COLOR_DEFAULT=$'\033[0;39m'
+IMOSH_COLOR_BLACK=$'\033[0;30m'
+IMOSH_COLOR_RED=$'\033[0;31m'
+IMOSH_COLOR_GREEN=$'\033[0;32m'
+IMOSH_COLOR_YELLOW=$'\033[0;33m'
+IMOSH_COLOR_BLUE=$'\033[0;34m'
+IMOSH_COLOR_MAGENTA=$'\033[0;35m'
+IMOSH_COLOR_CYAN=$'\033[0;36m'
+IMOSH_COLOR_WHITE=$'\033[0;37m'
