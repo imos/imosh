e6c089390c188e0d7de93bac7b6abba9370d4d52
imos <github@imoz.jp>
2014-09-18 01:47:29 +0900

    Rename imosh::internal::set to func::strcpy.

---
+++ library/20-func/strcpy.sh
@@ -0,0 +1,10 @@
+# Usage:
+#   func::strcpy destination source
+#
+# Assigns the content of a variable specified as source into destination.
+func::strcpy() {
+  local __strcpy_destination="$1"
+  local __strcpy_source="$2"
+
+  eval "${__strcpy_destination}=\"\${${__strcpy_source}}\""
+}
--- test/func/strcpy_test.sh
+++ test/func/strcpy_test.sh
@@ -1,11 +1,11 @@
-test::imosh_internal_set() {
+test::func_strcpy() {
   local source destination
 
   source='test'
-  imosh::internal::set destination source
+  func::strcpy destination source
   EXPECT_EQ 'test' "${destination}"
 
   source=$'abc\x00\x01\x02\x03\x04\x05\x06\x07\x08xyz'
-  imosh::internal::set destination source
+  func::strcpy destination source
   EXPECT_EQ $'abc\x00\x01\x02\x03\x04\x05\x06\x07\x08xyz' "${destination}"
 }
