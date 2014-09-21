0de166ea9ca8772b09b09b2880d2530bf641b639
imos <github@imoz.jp>
2014-09-21 14:30:01 +0900

    Use func::bin2hex instead of php::bin2hex.

--- test/func/bin2hex_test.sh
+++ test/func/bin2hex_test.sh
@@ -1,4 +1,8 @@
 test::func_bin2hex() {
-  EXPECT_EQ '686f6765' "$(print 'hoge' | php::bin2hex)"
+  EXPECT_EQ '686f6765' "$(print 'hoge' | func::bin2hex)"
   EXPECT_EQ 'e697a5e69cace8aa9e' "$(print '日本語' | func::bin2hex)"
+
+  local variable
+  func::bin2hex variable 'hoge'
+  EXPECT_EQ '686f6765' "${variable}"
 }
