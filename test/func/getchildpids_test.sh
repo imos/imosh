23750b4236a0c5dcf084f922806aa3497bf24723
imos <github@imoz.jp>
2014-09-24 03:29:59 +0900

    Make getchildpids' test have margins.

--- test/func/getchildpids_test.sh
+++ test/func/getchildpids_test.sh
@@ -1,12 +1,17 @@
 test::func_getchildpids() {
   local pids=()
   func::getchildpids pids
-  EXPECT_EQ 0 "${#pids[*]}"
+  EXPECT_TRUE [ "${#pids[*]}" -le 1 ]
 
-  sleep 10 &
-  local child_pid="$!"
+  local child_pids=()
+  for i in {1..10}; do
+    sleep 10 &
+    child_pids+=("$!")
+  done
   func::getchildpids pids
-  EXPECT_EQ 1 "${#pids[*]}"
+  EXPECT_TRUE [ "${#pids[*]}" -ge 10 -a "${#pids[*]}" -le 11 ]
 
-  kill -9 "${child_pid}"
+  for child_pid in "${child_pids[@]}"; do
+    kill -9 "${child_pid}"
+  done
 }
