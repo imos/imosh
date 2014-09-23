3363bb63cdd6f842c9afa8853c672001246e7436
imos <github@imoz.jp>
2014-09-23 18:13:30 +0900

    Remove php::str_replace.

--- library/20-php-str_replace.sh
+++
@@ -1,9 +0,0 @@
-# Usage:
-#   php::str_replace <search> <replace> <subject>
-php::str_replace() {
-  local search="${1}"
-  local replace="${2}"
-  local subject="${3}"
-
-  print "${subject//${search}/${replace}}"
-}
