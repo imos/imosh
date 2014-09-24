3f397ee56b35d786609ff0243bd0e10800660ff2
imos <github@imoz.jp>
2014-09-25 04:53:44 +0900

    Support reloading functions.

--- library/00-header/header.sh
+++ library/00-header/header.sh
@@ -1,8 +1,11 @@
+# Enables error checking if imosh is called in a script.
 if ! shopt login_shell >/dev/null; then
+  # Make a script fail when
+  # - a command returns non-zero value (-e).
+  # - an undefined variable is referred (-u).
   set -e -u
+  # Return if imosh is reloaded.
+  if [ "${__IMOSH_IS_LOADED+loaded}" = 'loaded' ]; then
+    return
+  fi
 fi
-
-if [ "${__IMOSH_IS_LOADED+loaded}" = 'loaded' ]; then
-  return
-fi
-__IMOSH_IS_LOADED=loaded
