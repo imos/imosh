1ebb3bc0bc548b68897f63db64be7199ae253302
imos <github@imoz.jp>
2014-10-07 01:27:52 +0900

    Make LOG output to the first STDERR.

--- library/70-setup/log.sh
+++ library/70-setup/log.sh
@@ -2,3 +2,5 @@
 exec 101>&- 102>&- 103>&- 104>&-
 # Open descriptors for LOG without calling init_log.
 exec 101>/dev/null 102>/dev/null 103>/dev/null 104>/dev/null
+# Redirect log output to the current STDERR.
+exec 105>&2
