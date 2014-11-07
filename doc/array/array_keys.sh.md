bdb7242a60bcd52ce46ed06e33e20b695f4b4b6f
imos <git@imoz.jp>
2014-11-08 03:18:46 +0900

    Update array_keys and implode.

--- doc/array/array_keys.sh.md
+++ doc/array/array_keys.sh.md
@@ -5,6 +5,8 @@ array_keys gets an array's keys.
 
 ## Usage
 ```sh
-// 1. Function form
+// 1. Function form.
 void func::array_keys(string[]* output, string[]* input)
+// 2. Command form.
+void sub::array_keys(string[]* input) > output
 ```
--- library/10-func/strings/implode.sh
+++ library/10-func/strings/implode.sh
@@ -8,12 +8,13 @@
 #     // 1. Function form.
 #     void func::implode(string* variable, string glue, string[]* pieces)
 #     // 2. Command form.
-#     void func::implode(string glue, string[]* pieces) > result
+#     void sub::implode(string glue, string[]* pieces) > result
 #     // 3. Stream form.
-#     void func::implode(string glue) < input > output
+#     void stream::implode(string glue) < input > output
 #
-# Alias:
-#   func::join is an alias of func::implode.
+# Aliases:
+#   func::join, sub::join and stream::join are aliases of func::implode,
+#   sub::implode and stream::implode respectively.
 func::implode() {
   # 1. Function form.
   if [ "$#" -eq 3 ]; then
@@ -33,27 +34,38 @@ func::implode() {
       (( __implode_i += 1 )) || true
     done
     func::let "${__implode_variable}" "${__implode_result}"
-  # 2. Command form
   elif [ "$#" -eq 2 ]; then
-    local __implode_output=''
-    # 1. Function form.
-    func::implode __implode_output "$1" "$2"
-    sub::println "${__implode_output}"
-    return
-  # 3. Stream form.
+    LOG ERROR 'This form is deprecated.'
+    sub::implode "$@"
   elif [ "$#" -eq 1 ]; then
-    local __implode_line=''
-    while read -r __implode_line; do
-      local pieces=(${__implode_line})
-      # 2. Command form.
-      func::implode "$1" pieces
-    done
-    return
-  # Argument mismatch.
+    LOG ERROR 'This form is deprecated.'
+    stream::implode "$@"
   else
-    LOG ERROR "The number of arguments does not match func::implode: $#"
-    return 1
+    eval "${IMOSH_WRONG_NUMBER_OF_ARGUMENTS}"
+  fi
+}
+
+sub::implode() {
+  if [ "$#" -eq 2 ]; then
+    local __implode_output=''
+    func::implode __implode_output "${1}" "${2}"
+    sub::println "${__implode_output}"
+  else
+    eval "${IMOSH_WRONG_NUMBER_OF_ARGUMENTS}"
+  fi
+}
+
+stream::implode() {
+  if [ "$#" -eq 1 ]; then
+    local LINE=()
+    while func::readarray; do
+      sub::implode "${1}" LINE
+    done
+  else
+    eval "${IMOSH_WRONG_NUMBER_OF_ARGUMENTS}"
   fi
 }
 
 func::join() { func::implode "$@"; }
+sub::join() { sub::implode "$@"; }
+stream::join() { stream::implode "$@"; }
