aa845e48e7e84cc947370b130dd461543c69ede6
imos <github@imoz.jp>
2014-11-17 04:06:29 +0900

    Refactoring.

--- library/10-internal/mktemp.sh
+++ library/10-internal/mktemp.sh
@@ -1,17 +1,10 @@
 __imosh::mktemp() {
   TMPDIR="${TMPDIR:-/tmp}"
   TMPDIR="${TMPDIR%/}"
-  local suffix=''
-  func::rand suffix 0 99999999
-  func::substr suffix "00000000${suffix}" -8
-  export TMPDIR="${TMPDIR}/imosh.${suffix}"
-  if ! mkdir -p "${TMPDIR}"; then
-    LOG FATAL "Failed to create a temporary directory: ${TMPDIR}"
-  fi
+  export TMPDIR="${TMPDIR}/imosh.${RANDOM}.${RANDOM}.${RANDOM}.${RANDOM}"
   export IMOSH_TMPDIR="${TMPDIR}"
-
   export __IMOSH_CORE_TMPDIR="${TMPDIR}/.imosh"
-  if ! mkdir "${__IMOSH_CORE_TMPDIR}"; then
+  if ! mkdir -p "${__IMOSH_CORE_TMPDIR}"; then
     LOG FATAL "Failed to create a temporary directory: ${__IMOSH_CORE_TMPDIR}"
   fi
 }
