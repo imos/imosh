a6d32b15e9f499eb859c1f09947855d9067338a7
imos <git@imoz.jp>
2014-11-26 01:29:23 +0900

    Refactoring mktemp.

--- library/70-setup/mktemp.sh
+++ library/70-setup/mktemp.sh
@@ -1,2 +1,9 @@
 # Prepares a temporary directory and sets its path to TMPDIR.
-__imosh::mktemp
+TMPDIR="${TMPDIR:-/tmp}"
+TMPDIR="${TMPDIR%/}"
+export TMPDIR="${TMPDIR}/imosh.${RANDOM}.${RANDOM}.${RANDOM}.${RANDOM}"
+export IMOSH_TMPDIR="${TMPDIR}"
+export __IMOSH_CORE_TMPDIR="${TMPDIR}/.imosh"
+if ! mkdir -p "${__IMOSH_CORE_TMPDIR}"; then
+  LOG FATAL "Failed to create a temporary directory: ${__IMOSH_CORE_TMPDIR}"
+fi
