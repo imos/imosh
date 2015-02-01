a0c901f6e801b54c7ae5a6fad26503b98382b876
imos <git@imoz.jp>
2015-02-01 20:04:11 +0900

    Improve strtotime.

--- library/10-func/datetime/strtotime.sh
+++ library/10-func/datetime/strtotime.sh
@@ -8,7 +8,13 @@ func::strtotime() {
   if [ "$#" -eq 2 ]; then
     local __func_strtotime_match=()
     local __func_strtotime_text=''
-    if [ "${#2}" -eq 10 ]; then
+    if [ "${#2}" -eq 8 ]; then
+      # 20060102
+      if sub::ereg_match '^([0-9]{8})$' "${2}"; then
+        __func::strtotime "${1}" "${2}000000" '%Y%m%d%H%M%S'
+        return
+      fi
+    elif [ "${#2}" -eq 10 ]; then
       # 2006-01-02
       if sub::ereg_match '^([0-9]{4})[-/:]([0-9]{2})[-/:]([0-9]{2})$' \
           "${2}" __func_strtotime_match; then
@@ -17,6 +23,12 @@ func::strtotime() {
         __func_strtotime_text+="-${__func_strtotime_match[3]}"
         __func_strtotime_text+=' 00:00:00'
         __func::strtotime "${1}" "${__func_strtotime_text}" '%Y-%m-%d %H:%M:%S'
+        return
+      fi
+    elif [ "${#2}" -eq 14 ]; then
+      # 20060102150405
+      if sub::ereg_match '^[0-9]{14}$' "${2}"; then
+        __func::strtotime "${1}" "${2}" '%Y%m%d%H%M%S'
         return
       fi
     elif [ "${#2}" -eq 19 ]; then
@@ -37,6 +49,90 @@ func::strtotime() {
         return
       fi
     fi
+    # Mon, 02 Jan 2006 15:04:05 GMT (HTTP format)
+    if sub::ereg_match \
+        '^([A-Z][a-z]+, +)?([0-9]+)[/ \-]+([A-Z][a-z][a-z])[/ \-]+([0-9]{2}|[0-9]{4})[ :T]([0-9]{2})[:\-]([0-9]{2})[:\-]([0-9]{2}) *([A-Z][A-Za-z0-9\-\+ /]+)?([\-\+][0-9]{4})?$' \
+        "${2}" __func_strtotime_match; then
+      local __func_strtotime_timezone=''
+      if [ "${__func_strtotime_match[8]}" != '' ]; then
+        __func_strtotime_timezone="${__func_strtotime_match[8]}"
+      elif [ "${__func_strtotime_match[9]}" != '' ]; then
+        __func_strtotime_timezone="${__func_strtotime_match[9]}"
+      fi
+      case "${__func_strtotime_timezone}" in
+        'JST') __func_strtotime_timezone='+0900';;
+      esac
+      if [ "${#__func_strtotime_match[4]}" -eq 2 ]; then
+        if [ "${__func_strtotime_match[4]}" -lt 70 ]; then
+          __func_strtotime_match[4]="20${__func_strtotime_match[4]}"
+        else
+          __func_strtotime_match[4]="19${__func_strtotime_match[4]}"
+        fi
+      fi
+      func::exec __func_strtotime_text printf '%02d %s %04d %02d:%02d:%02d' \
+          "${__func_strtotime_match[2]}" \
+          "${__func_strtotime_match[3]}" \
+          "${__func_strtotime_match[4]}" \
+          "${__func_strtotime_match[5]}" \
+          "${__func_strtotime_match[6]}" \
+          "${__func_strtotime_match[7]}"
+      if [ "${__func_strtotime_timezone}" != '' ]; then
+        if sub::ereg_match '^[\-\+][0-9]{4}$' \
+                           "${__func_strtotime_timezone}"; then
+          __func::strtotime \
+              "${1}" "${__func_strtotime_text} ${__func_strtotime_timezone}" \
+              '%d %b %Y %H:%M:%S %z'
+        else
+          TZ="${__func_strtotime_timezone}" \
+              __func::strtotime \
+              "${1}" "${__func_strtotime_text}" '%d %b %Y %H:%M:%S'
+        fi
+      else
+        __func::strtotime \
+            "${1}" "${__func_strtotime_text}" '%d %b %Y %H:%M:%S'
+      fi
+      return
+    fi
+    # 02/Jan/2006 (Common logfile format)
+    if sub::ereg_match \
+        '^([0-9]+)[/ \-]+([A-Z][a-z][a-z])[/ \-]+([0-9]{4})$' \
+        "${2}" __func_strtotime_match; then
+      func::exec __func_strtotime_text printf '%02d %s %04d 00:00:00' \
+          "${__func_strtotime_match[1]}" \
+          "${__func_strtotime_match[2]}" \
+          "${__func_strtotime_match[3]}"
+      __func::strtotime "${1}" "${__func_strtotime_text}" '%d %b %Y %H:%M:%S'
+      return
+    fi
+    # 02-Jan-06 (Old RFC850 HTTP format)
+    if sub::ereg_match \
+        '^([0-9]+)[/ \-]+([A-Z][a-z][a-z])[/ \-]+([0-9]{2})$' \
+        "${2}" __func_strtotime_match; then
+      func::exec __func_strtotime_text printf '%02d %s %02d 00:00:00' \
+          "${__func_strtotime_match[1]}" \
+          "${__func_strtotime_match[2]}" \
+          "${__func_strtotime_match[3]}"
+      __func::strtotime "${1}" "${__func_strtotime_text}" '%d %b %y %H:%M:%S'
+      return
+    fi
+    # Jan 2 2006 (Unix ls format)
+    if sub::ereg_match \
+        '^([A-Z][a-z][a-z])[/ \-]+([0-9]+)[,/ \-]+([0-9]{4})$' \
+        "${2}" __func_strtotime_match; then
+      func::exec __func_strtotime_text printf '%02d %s %04d 00:00:00' \
+          "${__func_strtotime_match[2]}" \
+          "${__func_strtotime_match[1]}" \
+          "${__func_strtotime_match[3]}"
+      __func::strtotime "${1}" "${__func_strtotime_text}" '%d %b %Y %H:%M:%S'
+      return
+    fi
+    # @1136214245 (Unix timestamp)
+    if sub::ereg_match '^@?(-?[0-9]+)$' "${2}" __func_strtotime_match; then
+      __func::strtotime "${1}" "${__func_strtotime_match[1]}" '%s'
+      return
+    fi
+    LOG ERROR "Unkown date format: ${2}"
+    return 1
   else
     eval "${IMOSH_WRONG_NUMBER_OF_ARGUMENTS}"
   fi
--- test/func/strtotime_test.sh
+++ test/func/strtotime_test.sh
@@ -8,10 +8,55 @@ run_datetime() {
   EXPECT_EQ '1136181845' "$(TZ=Asia/Tokyo sub::strtotime "${1}")"
 }
 
+run_fixed_datetime() {
+  EXPECT_EQ '1136214245' "$(TZ=UTC sub::strtotime "${1}")"
+  EXPECT_EQ '1136214245' "$(TZ=Asia/Tokyo sub::strtotime "${1}")"
+}
+
 test::strtotime() {
+  # Proposed new HTTP format.
+  run_date '02 Jan 2006'
+  # Old RFC850 HTTP format.
+  run_date '02-Jan-06'
+  # Broken RFC850 HTTP format.
+  run_date '02-Jan-2006'
+  # Common logfile format.
+  run_date '02/Jan/2006'
+  # ISO 8601 compact date format.
+  run_date '20060102'
+  # ISO 8601 date format.
   run_date '2006-01-02'
+  # UNIX ls format.
+  run_date 'Jan 2 2006'
+  # English date format.
+  run_date 'Jan 2, 2006'
+  # Japanese date format.
   run_date '2006/01/02'
+
+  # ISO 8601 format without timezone.
   run_datetime '2006-01-02 15:04:05'
+  # ISO 8601 format with a T separator.
   run_datetime '2006-01-02T15:04:05'
+  # Compact datetime format.
+  run_datetime '20060102150405'
+  # HTTP format without timezone.
+  run_datetime 'Mon, 02 Jan 2006 15:04:05'
+  run_datetime '02 Jan 2006 15:04:05'
+  # Japanese datetime format.
   run_datetime '2006/01/02 15:04:05'
+
+  # HTTP format.
+  run_fixed_datetime 'Mon, 02 Jan 2006 15:04:05 GMT'
+  run_fixed_datetime 'Mon, 02 Jan 2006 15:04:05 +0000'
+  run_fixed_datetime '02 Jan 2006 15:04:05 GMT'
+  run_fixed_datetime 'Tue, 03 Jan 2006 00:04:05 +0900'
+  run_fixed_datetime 'Tue, 03 Jan 2006 00:04:05 JST'
+  # Common logfile format.
+  run_fixed_datetime '03/Jan/2006:00:04:05 +0900'
+  # ISO 8601 format.
+  # run_fixed_datetime '2006-01-02 15:04:05 +0000'
+  # run_fixed_datetime '2006-01-03 00:04:05 +0900'
+  # UNIX timestamp format.
+  run_fixed_datetime '1136214245'
+  run_fixed_datetime '@1136214245'
 }
